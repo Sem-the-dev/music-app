35c5904851ba3d82d3baa7f80a58d3c910bd930f
sem-the-dev <semhar.tesfu@outlook.com>
2021-04-12 12:56:18 +0100

    add new files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Artists from './Artists'
+import './App.css'
+
+
+function App() {
+    return (
+<>
+<h1>A music app for all your fabourite tracks </h1>
+<Artists />
+</>
+    )
+}
+
+export default App;
---
+++ src/Artists.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import ReactDOM from 'react-dom'
+
+ function Artists(){
+    return (
+       <div>
+         <h2>artists</h2>
+       </div> 
+    )
+}
+
+export default Artists;
